171144d064857c293de29d449d3fc404a632c5f2
VDombard <v.dombard@gmail.com>
2020-04-30 16:29:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app"><h1>Gestion des destinations</h1>
+  <Destlist></Destlist>
+  </div>
+</template>
+
+<script>
+import Destlist from './Destlist';
+
+export default {
+  name: 'app',
+  components: {Destlist},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  background-color: skyblue;
+  margin: 10px auto;
+  padding: 10px;
+}
+
+</style>
---
+++ src/Destlist.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="destlist"> <h1>Liste des destinations</h1>
+  <div v-for="(dest, iddest) in destlist" :key="dest.idcountry">
+          {{dest.country}}
+  </div>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'Destlist',
+        data() {
+            return {
+                destlist: [],
+                url:"http://localhost:8000/api/homepage"
+            }
+        },
+        methods: {
+            get_destlist() {
+                axios
+                .get(this.url)
+                .then ( (response) => {
+                    this.destlist = response.data;
+                    console.log(this.destlist);
+                })
+                .catch( (error) => {
+                    console.log(error);
+                })
+            }
+        },
+        mounted() {
+            this.get_destlist();
+        }
+}
+</script>
+
+<style>
+#destlist {
+    background-color: lightblue;
+    margin: 10px auto;
+    padding: 10px;
+    width: 500px;
+}
+</style>
