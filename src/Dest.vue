8e01547e5a6cc63a252df4e9d204489840752fb9
VDombard <v.dombard@gmail.com>
2020-05-04 22:21:04 +0200

    CRUD

--- src/Dest.vue
+++ src/Dest.vue
@@ -1,5 +1,13 @@
 <template>
     <div id="dest"><h3>{{ dest.country }} </h3>
+    <button v-on:click="showInput = true" v-if="! showInput">Modifier</button>
+   
+        <div v-if="showInput">
+            <input type="text" v-model="dest.country">
+            <button v-on:click="updateDest(dest)">Sauver</button>
+        </div>
+        <button v-on:click="deleteDest(dest.idcountry)">Supprimer</button>
+
     </div>
 </template>
 
@@ -8,9 +16,19 @@
         name: "Dest",
         props:['dest'],
         data() {
-            return {}
+            return {
+                showInput:false
+            }
         },
-        methods: {}
+        methods: {
+            updateDest(dest) {
+                this.$emit('event_update', dest);
+                this.showInput = false;
+            },
+            deleteDest(iddest) {
+                this.$emit('event_delete', iddest);
+            }
+        }
     }
 </script>
 
--- src/Destlist.vue
+++ src/Destlist.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="destlist"> <h1>Liste des destinations</h1>
-  <div v-for="(dest, iddest) in destlist" :key="dest.idcountry">
-          {{ dest.country }}
-  </div>
+
+        <button v-on:click="showCreate = ! showCreate">Create destination</button>
+        <div v-if="showCreate">
+            <input type="text" v-model="dest.country">
+            <button v-on:click="destAdd">Sauver</button>
+        </div>
+
+        <div v-for="(dest, iddest) in destlist" :key="dest.idcountry">
+                <Dest v-bind:dest="dest" @event_update="destUpdate" @event_delete="destDelete"></Dest>
+        </div>
+
   </div>
 </template>
 
 <script>
     import axios from "axios";
-    //import dest from "./Dest";
+    import Dest from "./Dest";
 
     export default {
         name: "Destlist",
-        //components: { Dest },
+        components: { Dest },
         data() {
             return {
+                dest: {
+                    id: 0,
+                    country:"none",
+                },
                 destlist: [],
-                url:"http://localhost:8000/api/homepage"
+                url:"http://localhost:8000/api/homepage",
+                showCreate: false
             }
         },
         methods: {
@@ -30,6 +43,35 @@
                 .catch( (error) => {
                     console.log(error);
                 })
+            },
+            destUpdate(dest) {
+                axios.put(this.url + dest.idcountry, dest)
+                .then( (reponse) => {
+                    console.log(response.date);
+                    })
+                .catch( (error) => {
+                    console.log(error);
+                })  
+            },
+            destAdd() {
+                axios.post(this.url, this.dest)
+                .then ( (response) => {
+                    console.log(response.data.destination);
+                    this.get_destlist();
+                })
+                .catch( (error) => {
+                    console.log(error);
+                })
+            },
+            destDelete(id) {
+                axios.delete(this.url + id)
+                .then ( (response) => {
+                    console.log(response.data.destination);
+                    this.get_destlist();
+                })
+                .catch( (error) => {
+                    console.log(error); 
+                })                
             }
         },
         mounted() {
